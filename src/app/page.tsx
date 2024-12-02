79377b73a572fa669b228f84e951e220f0585f65
BrentSoler <devbrent03@gmail.com>
2024-12-02 08:26:58 +0800

    FE: added carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import CustomCarousel from "@/components/custom-carousel/CustomCarousel";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="flex justify-center p-4">
+        <CustomCarousel />
+      </div>
+    </div>
+  );
 }
